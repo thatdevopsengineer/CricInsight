506c76208690bcc77e276d8474c6f0ad549351de
Daoud-Hussain <daoudhussain302@gmail.com>
2024-07-25 00:42:42 +0500

    Create profile Ui

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,14 +1,103 @@
-import React from 'react';
-import { Box, Typography } from '@mui/material';
+import React from "react";
+import { TextField, Button, Avatar, Grid, Typography, Container, Box } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-export default function Profile() {
+const theme = createTheme({
+  typography: {
+    fontFamily: "Poppins, sans-serif",
+  },
+  palette: {
+    primary: {
+      main: "#030947",
+    },
+  },
+});
+
+const ProfileEdit = () => {
   return (
-    
-      <Box
-        component="img"
-        src="/areas.png"
-        alt="Profile"
-        sx={{ width: '100%', height: 'auto'}}
-      />
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="md">
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Typography component="h1" variant="h5" alignSelf="flex-start" fontWeight="600">
+            Edit profile
+          </Typography>
+          <Box component="form" noValidate sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="fname"
+                  name="firstName"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="First Name"
+                  autoFocus
+                  placeholder="Mehrab"
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="Last Name"
+                  name="lastName"
+                  autoComplete="lname"
+                  placeholder="Bozorgi"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email"
+                  name="email"
+                  autoComplete="email"
+                  placeholder="Mehrabbozorgi.business@gmail.com"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                  placeholder="33062 Zboncak isle"
+                />
+              </Grid>
+              <Grid item xs={12} display="flex" justifyContent="flex-end">
+               
+              </Grid>
+            </Grid>
+            <Box display="flex" justifyContent="space-between" sx={{ mt: 3 }}>
+              <Box>
+                <Button variant="outlined" sx={{ mr: 2 }}>
+                  Cancel
+                </Button>
+                <Button type="submit" variant="contained" color="primary">
+                  Save
+                </Button>
+              </Box>
+              <Button type="submit" variant="contained" color="primary" sx={{background: '#D52728'}}>
+                Delete
+              </Button>
+            </Box>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
   );
 }
+
+export default ProfileEdit;
