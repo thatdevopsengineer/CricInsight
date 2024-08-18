dac6c8a394fbab6c0dcb9f23bf5fdf18670cceb7
Daoud-Hussain <daoudhussain302@gmail.com>
2024-08-18 18:59:28 +0500

    customize barlines

--- frontend/src/components/Visualization.jsx
+++ frontend/src/components/Visualization.jsx
@@ -3,22 +3,22 @@ import { Box, Typography, Grid, Button } from '@mui/material';
 import { PieChart, Pie, Cell, BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
 const data = [
-  { name: 'Cover Drive', value: 15, color: '#FF4136' },
-  { name: 'Cut', value: 9, color: '#FF851B' },
-  { name: 'Flick', value: 33, color: '#7FDBFF' },
-  { name: 'Sweep', value: 5, color: '#001f3f' },
-  { name: 'Pull', value: 18, color: '#2ECC40' },
-  { name: 'Others', value: 20, color: '#FFDC00' },
+  { name: 'Cover Drive', value: 15, color: '#F94144' }, 
+  { name: 'Cut', value: 9, color: '#F8961E' },          
+  { name: 'Flick', value: 33, color: '#2D9CDB' },       
+  { name: 'Sweep', value: 5, color: '#030947' },        
+  { name: 'Pull', value: 18, color: '#90BE6D' },        
+  { name: 'Others', value: 20, color: '#F3722C' },      
 ];
 
 const Visualization = () => {
   return (
-    <Box sx={{ padding: '20px', backgroundColor: '#f5f5f5', borderRadius: '8px' }}>
+    <Box sx={{ px: '20px', py: '30px' ,borderRadius: '8px' }}>
       <Typography variant="h5" align="center" sx={{ marginBottom: '20px', color: '#34495e', fontWeight: 'bold' }}>
         Shots Visualization
       </Typography>
 
-      <Grid container spacing={2} justifyContent="center" sx={{marginBottom: '20px' }}>
+      <Grid container spacing={2} justifyContent="center" sx={{ marginBottom: '20px' }}>
         {data.map((shot) => (
           <Grid item key={shot.name}>
             <Button
@@ -38,23 +38,24 @@ const Visualization = () => {
         ))}
       </Grid>
 
-      <Grid container spacing={4}>
-        <Grid item xs={12} md={6}>
-          <BarChart width={400} height={250} data={data}>
+      <Grid container spacing={4} sx={{ paddingTop: '20px', minHeight: '80vh' }}>
+        <Grid item xs={12} md={8}>
+          <BarChart width={600} height={400} data={data} borderRadius="20">
             <CartesianGrid strokeDasharray="3 3" />
             <XAxis dataKey="name" />
             <YAxis />
             <Tooltip />
             <Legend />
-            <Bar dataKey="value">
+            <Bar dataKey="value" >
               {data.map((entry, index) => (
                 <Cell key={`cell-${index}`} fill={entry.color} />
               ))}
             </Bar>
           </BarChart>
         </Grid>
-        <Grid item xs={12} md={3}>
-          <PieChart width={200} height={200}>
+
+        <Grid item xs={12} md={4} >
+          <PieChart width={250} height={250}>
             <Pie
               data={data}
               dataKey="value"
@@ -69,24 +70,28 @@ const Visualization = () => {
               ))}
             </Pie>
           </PieChart>
-        </Grid>
-        <Grid item xs={12} md={3}>
-          <PieChart width={200} height={200}>
+
+          <PieChart width={250} height={250}>
             <Pie
               data={data}
-              dataKey="value"
               cx="50%"
               cy="50%"
+              innerRadius={45}
               outerRadius={80}
               fill="#8884d8"
-              label
+              paddingAngle={5}
+              dataKey="value"
+              label={({ percent }) => `${(percent * 100).toFixed(0)}%`}
             >
               {data.map((entry, index) => (
                 <Cell key={`cell-${index}`} fill={entry.color} />
               ))}
             </Pie>
+            <Tooltip />
           </PieChart>
         </Grid>
+
+       
       </Grid>
     </Box>
   );
