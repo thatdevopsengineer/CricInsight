343b8610707fc778da1c5fb736a809893d218056
Daoud-Hussain <dev.daoudhussain@gmail.com>
2024-12-17 16:26:25 +0500

    Test change triggerd

--- backend/controllers/videoController.js
+++ backend/controllers/videoController.js
@@ -28,7 +28,7 @@ const awsConfig = require('../awsConfig');
 
 // Configure AWS (add these lines)
 AWS.config.update({
-  region: 'ap-south-1', // e.g., 'us-east-1'
+  region: 'ap-south-1', 
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 });
